7449dbb556eb9129fcf89dc6d4a50f7ac6e3147d
Igor Sarnowski <igor.sarnowski@gmail.com>
2020-11-22 19:39:16 +0100

    Bazowa logika

--- SitoErastostenesa/Form1.Designer.cs
+++ SitoErastostenesa/Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbZakres = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPrzesiej = new System.Windows.Forms.Button();
+            this.lblTablica = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // tbZakres
+            // 
+            this.tbZakres.Location = new System.Drawing.Point(90, 15);
+            this.tbZakres.Name = "tbZakres";
+            this.tbZakres.Size = new System.Drawing.Size(143, 20);
+            this.tbZakres.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Podaj zakres:";
+            // 
+            // btnPrzesiej
+            // 
+            this.btnPrzesiej.Location = new System.Drawing.Point(12, 44);
+            this.btnPrzesiej.Name = "btnPrzesiej";
+            this.btnPrzesiej.Size = new System.Drawing.Size(221, 23);
+            this.btnPrzesiej.TabIndex = 2;
+            this.btnPrzesiej.Text = "Przesiej";
+            this.btnPrzesiej.UseVisualStyleBackColor = true;
+            this.btnPrzesiej.Click += new System.EventHandler(this.btnPrzesiej_Click);
+            // 
+            // lblTablica
+            // 
+            this.lblTablica.AutoSize = true;
+            this.lblTablica.Location = new System.Drawing.Point(13, 70);
+            this.lblTablica.MaximumSize = new System.Drawing.Size(220, 0);
+            this.lblTablica.MinimumSize = new System.Drawing.Size(220, 0);
+            this.lblTablica.Name = "lblTablica";
+            this.lblTablica.Size = new System.Drawing.Size(220, 13);
+            this.lblTablica.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(239, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(292, 423);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(546, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblTablica);
+            this.Controls.Add(this.btnPrzesiej);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbZakres);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sito Erastostenesa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbZakres;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPrzesiej;
+        private System.Windows.Forms.Label lblTablica;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- SitoErastostenesa/Form1.cs
+++ SitoErastostenesa/Form1.cs
@@ -16,5 +16,45 @@ namespace SitoErastostenesa
         {
             InitializeComponent();
         }
+
+        private void Sito(bool[] tab, int n)
+        {
+            for (int i = 2; i * i <= n; i++) //przeszukujemy kolejnych kandydatów na pierwsze 
+            {
+                if (tab[i]) {
+                    for (int j = i * i; j <= n; j += i)
+                        tab[j] = false;
+                }
+            }
+        }
+
+        private void btnPrzesiej_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(tbZakres.Text);
+
+            if (n > 50) return;
+
+            bool[] tab = new bool[n+1];
+
+            for (int i = 2; i <= n; i++)
+                tab[i] = true;
+
+            Sito(tab, n); //przesianie liczb 
+
+            string wynik = $"Kolejne liczby pierwsze z przedziału [2..{n}]: ";
+
+            for (int i = 2; i <= n; i++)
+                if (tab[i])
+                    wynik += $"{i} ";
+
+            lblTablica.Text = wynik;
+
+            PokarzSchemat();
+        }
+
+        private void PokarzSchemat()
+        {
+
+        }
     }
 }
